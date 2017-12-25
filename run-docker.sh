3ecd8cb59cc56af5d96ded97e8435ba049797ba1
Akira Sato <satoach.net@example.com>
2017-12-26 00:05:22 +0900

    add doxygen-pdf Dockerfile

---
+++ run-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE="sakirror/imagename"
+DOCKFILE="./path/to/Dockerfile"
+VOLPATH_HOST="${PWD}/path/to/volume"
+VOLPATH_GUEST="/work"
+
+function run_sphinx()
+{
+    docker images | grep $IMAGE > /dev/null
+
+    if [ $? -ne 0 ]; then
+        docker build -t $IMAGE $DOCKFILE
+    fi
+
+    docker run --rm -it -v $VOLPATH_HOST:$VOLPATH_GUEST $IMAGE $@
+}
+
+run_sphinx $@
